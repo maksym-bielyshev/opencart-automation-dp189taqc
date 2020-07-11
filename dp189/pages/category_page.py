bd6dd1da2a5947278be17ad4b5f51cf99aaae81b
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-07-11 19:56:15 +0300

    Docstrings and type hints are added.

--- dp189/pages/category_page.py
+++ dp189/pages/category_page.py
@@ -1,10 +1,19 @@
+"""Category page."""
+
+from selenium.webdriver import Remote
 from dp189.pages.base_page import BasePage
 from dp189.components import LeftCategoryMenuComponent, SortByDropdownComponent, ShowNumberProductsDropdownComponent, \
     ListViewComponent, GridViewComponent, CategoryProductWidgetComponent, ProductCompareLinkComponent
 
 
 class CategoryPage(BasePage):
-    def __init__(self, driver):
+    """Category page class."""
+
+    def __init__(self, driver: Remote) -> None:
+        """All objects on product page.
+
+        :param driver: Remote
+        """
         super().__init__(driver)
         self.category_title = driver.find_element_by_id("content")
         self.left_category_menu = LeftCategoryMenuComponent(driver)
