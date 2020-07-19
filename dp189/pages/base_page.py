ec1037f98905e868e93d8b801c495b430511674b
AlexDnipro <olexiyusov@gmail.com>
2020-07-19 08:34:52 +0300

    added attributes to basepage class

--- dp189/pages/base_page.py
+++ dp189/pages/base_page.py
@@ -2,22 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 #MacOS imports
-from ..components import SearchArea, BaseNavBar, ShopCartButton
-from ..locators import LocatorsShoppingCartButton, LocatorYourStoreLink, LocatorsNavBar
+from ..components import BasePageNavBarComponent, ShopCartButtonComponent, InputFieldComponent, DropdownComponent
+from ..locators import LocatorsShoppingCartButton, LocatorYourStoreLink, LocatorsBasePageNavBar, LocatorBasePageSearch
+from ..locators import LocatorsBasePageMainMenu
 
 
 class BasePage:
     def __init__(self, driver):
         self._driver = driver
-        self.top_nav_bar = BaseNavBar(driver)
-        self.my_account = driver.find_element(*LocatorsNavBar.MY_ACCOUNT)
+        self.top_nav_bar = BasePageNavBarComponent(driver)
+        self.main_menu_desktops = DropdownComponent(driver, LocatorsBasePageMainMenu.DESKTOPS)
+        self.main_menu_laptops_notebooks = DropdownComponent(driver, LocatorsBasePageMainMenu.LAPTOPS_NOTEBOOKS)
+        self.main_menu_components = DropdownComponent(driver, LocatorsBasePageMainMenu.COMPONENTS)
+        self.main_menu_tablets = DropdownComponent(driver, LocatorsBasePageMainMenu.TABLETS)
+        self.main_menu_software = DropdownComponent(driver, LocatorsBasePageMainMenu.SOFTWARE)
+        self.main_menu_phones_pdas = DropdownComponent(driver, LocatorsBasePageMainMenu.PHONES_PDAS)
+        self.main_menu_cameras = DropdownComponent(driver, LocatorsBasePageMainMenu.CAMERAS)
+        self.main_menu_mp3players = DropdownComponent(driver, LocatorsBasePageMainMenu.MP3_PLAYERS)
+        self.my_account = driver.find_element(*LocatorsBasePageNavBar.MY_ACCOUNT)
         self.your_store_link = driver.find_element(*LocatorYourStoreLink.YOUR_STORE)
-        self.search = SearchArea(driver)
-        self.shop_cart_button = ShopCartButton(driver)
+        self.search_field = InputFieldComponent(driver, LocatorBasePageSearch.SEARCH_FIELD)
+        self.shop_cart_button = ShopCartButtonComponent(driver)
 
-    def click_my_account(self):
-        self.my_account.click()
-    
     def click_account_and_go_to_login(self):
         self.my_account.click()
         self.my_account.find_element(*LocatorsNavBar.LOGIN).click()
