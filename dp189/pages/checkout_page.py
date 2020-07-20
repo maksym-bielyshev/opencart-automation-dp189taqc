833e8e409e669b4f624d99fec4e91fb5cdeb34dd
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-07-21 00:53:43 +0300

    Add page skeleton

---
+++ dp189/pages/checkout_page.py
@@ -0,0 +1,26 @@
+class CheckoutOptions:
+    pass
+
+
+class BillingDetails:
+    pass
+
+
+class AccountAndBillingDetails:
+    pass
+
+
+class DeliveryDetails:
+    pass
+
+
+class DeliveryMethod:
+    pass
+
+
+class PaymentMethod:
+    pass
+
+
+class ConfirmOrder:
+    pass
