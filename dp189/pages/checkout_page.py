ed28a49cfceb906f7ff1abd1e08374d23989ae08
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-07-21 02:11:43 +0300

    Create 'CheckoutOptions' class

--- dp189/pages/checkout_page.py
+++ dp189/pages/checkout_page.py
@@ -1,10 +1,33 @@
 from selenium.webdriver import Remote
+from dp189.pages.base_page import BasePage
 from dp189.locators import LocatorsCheckoutPage
+from dp189.components import InputFieldComponent
 
 
-class CheckoutOptions:
+class CheckoutOptions(BasePage):
     def __init__(self, driver: Remote) -> None:
+        super().__init__(driver)
         self._driver = driver
+        self.email_field_returning_customer = \
+            InputFieldComponent(self._driver, LocatorsCheckoutPage.EMAIL_FIELD_RETURNING_CUSTOMER)
+        self.password_field_returning_customer = \
+            InputFieldComponent(self._driver, LocatorsCheckoutPage.PASSWORD_FIELD_RETURNING_CUSTOMER)
+
+    def click_register_account_radio_button(self):
+        self._driver.find_element(LocatorsCheckoutPage.REGISTER_ACCOUNT_RADIO_BUTTON).click()
+
+    def click_guest_checkout_radio_button(self):
+        self._driver.find_element(LocatorsCheckoutPage.GUEST_CHECKOUT_RADIO_BUTTON).click()
+
+    def click_continue_button(self):
+        self._driver.find_element(LocatorsCheckoutPage.CONTINUE_BUTTON).click()
+
+    def click_forgotten_password_link(self):
+        self._driver.find_element(LocatorsCheckoutPage.FORGOTTEN_PASSWORD_LINK).click()
+
+    def click_login_button(self):
+        self._driver.find_element(LocatorsCheckoutPage.LOGIN_BUTTON).click()
+
 
 class BillingDetails:
     def __init__(self, driver: Remote) -> None:
