e38bef91c68a175b9100c50a2b0dcc9b94a6008d
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-07-20 10:58:03 +0300

    Remove the import of the page from the main level

--- dp189/components.py
+++ dp189/components.py
@@ -14,8 +14,6 @@ from dp189.locators import LocatorsSearch, LocatorsNavBar, RightMenuLocators, Lo
     LocatorsNewsletterComponent, LocatorsAddAddressComponent, LocatorsNewsletterComponent, LocatorsSearch, LocatorsNavBar, RightMenuLocators, LocatorsShoppingCartButton, \
     LocatorProductCompareLink, LocatorsViewModeButton, LocatorProductWidget, LocatorsInfoMessages
 
-from dp189.pages.compare_page import ComparePage
-
 
 class SearchArea:
     """"This class describes the search area common in all pages. It consists or search field and search button"""
