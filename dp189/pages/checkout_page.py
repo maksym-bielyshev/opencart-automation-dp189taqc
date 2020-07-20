893032af4dcb5a1ece0b6bf5d26753060597750e
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-07-21 02:13:01 +0300

    Add 'super()' call for all classes

--- dp189/pages/checkout_page.py
+++ dp189/pages/checkout_page.py
@@ -31,29 +31,35 @@ class CheckoutOptions(BasePage):
 
 class BillingDetails:
     def __init__(self, driver: Remote) -> None:
+        super().__init__(driver)
         self._driver = driver
 
 
 class AccountAndBillingDetails:
     def __init__(self, driver: Remote) -> None:
+        super().__init__(driver)
         self._driver = driver
 
 
 class DeliveryDetails:
     def __init__(self, driver: Remote) -> None:
+        super().__init__(driver)
         self._driver = driver
 
 
 class DeliveryMethod:
     def __init__(self, driver: Remote) -> None:
+        super().__init__(driver)
         self._driver = driver
 
 
 class PaymentMethod:
     def __init__(self, driver: Remote) -> None:
+        super().__init__(driver)
         self._driver = driver
 
 
 class ConfirmOrder:
     def __init__(self, driver: Remote) -> None:
+        super().__init__(driver)
         self._driver = driver
