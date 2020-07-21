5ef4e284fecf1a5b169284c49d77c75cae66eb0f
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-07-21 08:12:36 +0300

    Create 'setup' and 'teardown' methods

---
+++ dp189/tests/dpoc_272_checkout_page_guest_checkout_with_valid_data.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from dp189.pages.checkout_page import CheckoutOptions
+from dp189.components import ProductWidgetComponent
+options = Options()
+options.add_argument('--ignore-certificate-errors')
+import time
+
+
+class TestCheckoutPage():
+    def setup(self):
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.maximize_window()
+        self.driver.get('https://34.71.14.206/index.php')
+        ProductWidgetComponent(self.driver, 'iPhone').click_add_to_shopping_cart_button()
+        self.driver.get('https://34.71.14.206/index.php?route=checkout/checkout')
+        self.register_page = CheckoutOptions(self.driver)
+        time.sleep(3)
+
+    def test_guest_checkout_with_valid_data(self):
+        pass
+
+    def teardown(self):
+        self.driver.close()
