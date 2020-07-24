7d078704537790c0195770f0ac176a6af237f5e2
Tanya-Kr <tanya.krug7@gmail.com>
2020-07-24 15:20:54 +0300

    Add BaseTest to TestShoppingCart

--- dp189/tests/test_ftc_shopping_cart_page.py
+++ dp189/tests/test_ftc_shopping_cart_page.py
@@ -5,34 +5,36 @@ from dp189.pages.product_page import ProductPage
 from dp189.pages.shopping_cart_page import ShoppingCartPage
 import csv
 
+from dp189.tests.base_test import BaseTest
+
 CHROME_DRIVER_PATH = 'C:/Users/Home/Downloads/chromedriver_win32/chromedriver.exe'
 PRODUCT_URL = 'http://34.71.14.206/index.php?route=product/product&product_id=40'
 SHOPPING_CART_URL = 'http://34.71.14.206/index.php?route=checkout/cart'
 
 
-def get_test_data():
-    with open('../testsData/test_data_shopping_cart_quantity.csv', 'r') as file:
-        reader = csv.reader(file, quoting=csv.QUOTE_ALL, skipinitialspace=True, delimiter='\t')
-        test_data_list = []
-        for row in reader:
-            test_data_list.append(tuple(row[0].strip('][').split(';')))
-        return test_data_list
+# def get_test_data():
+#     with open('../testsData/test_data_shopping_cart_quantity.csv', 'r') as file:
+#         reader = csv.reader(file, quoting=csv.QUOTE_ALL, skipinitialspace=True, delimiter='\t')
+#         test_data_list = []
+#         for row in reader:
+#             test_data_list.append(tuple(row[0].strip('][').split(';')))
+#         return test_data_list
 
 
-class TestShoppingCart:
+class TestShoppingCart(BaseTest):
     def setup(self):
-        options = Options()
-        options.add_argument('--ignore-certificate-errors')
-        self._driver = webdriver.Chrome(CHROME_DRIVER_PATH, options=options)
-        self._driver.maximize_window()
+        # options = Options()
+        # options.add_argument('--ignore-certificate-errors')
+        # self._driver = webdriver.Chrome(CHROME_DRIVER_PATH, options=options)
+        self.driver.maximize_window()
 
-        self._driver.get(PRODUCT_URL)
-        product = ProductPage(self._driver)
+        self.driver.get(PRODUCT_URL)
+        product = ProductPage(self.driver)
         product.available_options.click_add_to_cart_button()
 
-        self._driver.get(SHOPPING_CART_URL)
+        self.driver.get(SHOPPING_CART_URL)
 
-        self.cart = ShoppingCartPage(self._driver)
+        self.cart = ShoppingCartPage(self.driver)
         self.cart.generate_products_list()
 
     def test_shopping_cart_change_quantity_positive(self):
@@ -43,7 +45,8 @@ class TestShoppingCart:
 
         assert 'Success: You have modified your shopping cart!' in self.cart.catch_info_message.get_info_message()
 
-    @pytest.mark.parametrize('test_input,expected', get_test_data())
+    @pytest.mark.parametrize('test_input,expected',
+                             BaseTest.get_test_data('test_data_shopping_cart_quantity.csv'))
     def test_shopping_cart_change_quantity_negative(self, test_input: str, expected: str):
         """Negative test for checking changes in the cart when invalid data is entered.
         :param test_input: str
@@ -52,6 +55,3 @@ class TestShoppingCart:
         self.cart = self.cart.change_product_quantity('iPhone', test_input)
 
         assert self.cart.get_text_empty_cart() == expected
-
-    def teardown(self):
-        self._driver.close()
