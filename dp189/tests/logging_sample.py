5ec0c60279630e2b6227e6784f74b5b419f0e2bd
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-07-24 19:46:31 +0300

    Change quote marks style

--- dp189/tests/logging_sample.py
+++ dp189/tests/logging_sample.py
@@ -7,8 +7,8 @@ __log = logging.getLogger(__name__)
 
 def debug_function_example():
     # Create log points with message level and string info.
-    __log.debug("Start function.")
-    print("Function flow.")
-    __log.debug("End function.")
+    __log.debug('Start function.')
+    print('Function flow.')
+    __log.debug('End function.')
 
 # Run this function in main.py module and go to the 'dp189/tests/logs/logging_sample.log' for information.
