a69279084ace3dbc0338172372322f48f67cd92d
Tanya-Kr <tanya.krug7@gmail.com>
2020-07-24 19:46:21 +0300

    Change variables name

--- dp189/routes.py
+++ dp189/routes.py
@@ -4,35 +4,35 @@ ROUTE_PRODUCT = '/index.php?route=product'
 ROUTE_CHECKOUT = '/index.php?route=checkout'
 ROUTE_ACCOUNT = '/index.php?route=account'
 
-HOME_PAGE = PROTOCOL + DOMAIN + '/index.php?route=common/home'
+HOME_PAGE_URL = f'{PROTOCOL}{DOMAIN}/index.php?route=common/home'
 
-CART = PROTOCOL + DOMAIN + ROUTE_CHECKOUT + '/cart'
-CHECKOUT = PROTOCOL + DOMAIN + ROUTE_CHECKOUT + '/checkout'
+CART_PAGE_URL = f'{PROTOCOL}{DOMAIN}{ROUTE_CHECKOUT}/cart'
+CHECKOUT_PAGE_URL = f'{PROTOCOL}{DOMAIN}{ROUTE_CHECKOUT}/checkout'
 
-WISH_LIST = PROTOCOL + DOMAIN + ROUTE_ACCOUNT + '/wishlist'
-REGISTER = PROTOCOL + DOMAIN + ROUTE_ACCOUNT + '/register'
-LOGIN = PROTOCOL + DOMAIN + ROUTE_ACCOUNT + '/login'
-ACCOUNT = PROTOCOL + DOMAIN + ROUTE_ACCOUNT + '/account'
-EDIT_ACCOUNT = PROTOCOL + DOMAIN + ROUTE_ACCOUNT + '/edit'
-PASSWORD = PROTOCOL + DOMAIN + ROUTE_ACCOUNT + '/password'
-ADDRESS = PROTOCOL + DOMAIN + ROUTE_ACCOUNT + '/address'
-ADDRESS_ADD = PROTOCOL + DOMAIN + ROUTE_ACCOUNT + '/address/add'
-ORDER_HISTORY = PROTOCOL + DOMAIN + ROUTE_ACCOUNT + '/order'
-LOGOUT = PROTOCOL + DOMAIN + ROUTE_ACCOUNT + '/logout'
-GIFT_CERTIFICATE = PROTOCOL + DOMAIN + ROUTE_ACCOUNT + '/voucher'
-
-
-def get_product_url(product_id):
-    return PROTOCOL + DOMAIN + ROUTE_PRODUCT + f'/product&product_id={product_id}'
+REGISTER_PAGE_URL = f'{PROTOCOL}{DOMAIN}{ROUTE_ACCOUNT}/register'
+LOGIN_PAGE_URL = f'{PROTOCOL}{DOMAIN}{ROUTE_ACCOUNT}/login'
+ACCOUNT_PAGE_URL = f'{PROTOCOL}{DOMAIN}{ROUTE_ACCOUNT}/account'
+EDIT_ACCOUNT_PAGE_URL = f'{PROTOCOL}{DOMAIN}{ROUTE_ACCOUNT}/edit'
+CHANGE_PASSWORD_PAGE_URL = f'{PROTOCOL}{DOMAIN}{ROUTE_ACCOUNT}/password'
+ADDRESS_BOOK_PAGE_URL = f'{PROTOCOL}{DOMAIN}{ROUTE_ACCOUNT}/address'
+ADDRESS_ADD_PAGE_URL = f'{PROTOCOL + DOMAIN}{ROUTE_ACCOUNT}/address/add'
+WISH_LIST_PAGE_URL = f'{PROTOCOL}{DOMAIN}{ROUTE_ACCOUNT}/wishlist'
+ORDER_HISTORY_PAGE_URL = f'{PROTOCOL}{DOMAIN}{ROUTE_ACCOUNT}/order'
+LOGOUT_PAGE_URL = f'{PROTOCOL}{DOMAIN}{ROUTE_ACCOUNT}/logout'
+GIFT_CERTIFICATE_PAGE_URL = f'{PROTOCOL}{DOMAIN}{ROUTE_ACCOUNT}/voucher'
 
 
-def get_search_page_url(search_query):
-    return PROTOCOL + DOMAIN + ROUTE_PRODUCT + f'/search&search={search_query}'
+def get_product_url(product_id: str) -> str:
+    return f'{PROTOCOL}{DOMAIN}{ROUTE_PRODUCT}/product&product_id={product_id}'
 
 
-def get_category_url(category_id):
-    return PROTOCOL + DOMAIN + ROUTE_PRODUCT + f'/category&path={category_id}'
+def get_search_page_url(search_query: str) -> str:
+    return f'{PROTOCOL}{DOMAIN}{ROUTE_PRODUCT}/search&search={search_query}'
 
 
-def get_edit_address_url(address_id):
-    return ADDRESS + f'/edit&address_id={address_id}'
+def get_category_url(category_id: str) -> str:
+    return f'{PROTOCOL}{DOMAIN}{ROUTE_PRODUCT}/category&path={category_id}'
+
+
+def get_edit_address_url(address_id: str) -> str:
+    return f'{ADDRESS_BOOK_PAGE_URL}/edit&address_id={address_id}'
