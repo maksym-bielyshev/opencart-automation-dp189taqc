e3a1891d0108b8e5e8c0d9cf15bb8176f1bd2d09
Diana Kardashova <kardashova.1999@gmail.com>
2020-07-24 11:10:42 +0300

    Refactor test_ftc_register_page.py

--- dp189/tests/test_ftc_register_page.py
+++ dp189/tests/test_ftc_register_page.py
@@ -1,26 +1,90 @@
 import pytest
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 from dp189.pages.register_page import RegisterPage
 options = Options()
 options.add_argument('--ignore-certificate-errors')
+options.add_argument('headless')
 
 
-class TestAddUser:
+class TestYourPersonalDetails:
+    """Tests for YourPersonalDetails component which consists four input field: First Name, Last Name, E-Mail, Telephone."""
 
     def setup(self):
         self.driver = webdriver.Chrome(options=options)
         self.driver.maximize_window()
-        self.driver.get('https://34.71.14.206/index.php?route=account/register')
+        self.driver.get('http://34.71.14.206/index.php?route=account/register')
         self.register_page = RegisterPage(self.driver)
 
     @pytest.mark.parametrize('first_name, expected_result',
                              [
-                                 ('', 'First Name must be between 1 and 32 characters!'),
-                                 ('111111111111111111111111111111111', 'First Name must be between 1 and 32 characters!')
+                                 ('W', 'selenium.common.exceptions.TimeoutException: Message:'),
+                                 ('Wolfeschlegelste', 'selenium.common.exceptions.TimeoutException: Message:'),
+                                 ('Wolfeschlegelsteinhausenbergerdo',
+                                  'selenium.common.exceptions.TimeoutException: Message:')
                              ]
                              )
-    def test_first_name_field_wrong_number_characters(self, first_name, expected_result):
+    def test_first_name_field_valid_number_characters(self, first_name, expected_result):
+        with pytest.raises(TimeoutException) as error:
+            self.register_page.your_personal_details_form.first_name_field.clear_and_fill_input_field(first_name)
+            self.register_page.click_continue_button()
+            self.register_page.your_personal_details_form.first_name_field.error_message.get_error_message()
+        assert str(TimeoutException) in str(error.type)
+
+    @pytest.mark.parametrize('last_name, expected_result',
+                             [
+                                 ('E', 'selenium.common.exceptions.TimeoutException: Message:'),
+                                 ('Enraejakavarapa', 'selenium.common.exceptions.TimeoutException: Message:'),
+                                 ('Enraejakavarapantiyacuppiramaniy',
+                                  'selenium.common.exceptions.TimeoutException: Message:')
+                             ]
+                             )
+    def test_last_name_field_valid_number_characters(self, last_name, expected_result):
+        with pytest.raises(TimeoutException) as error:
+            self.register_page.your_personal_details_form.last_name_field.clear_and_fill_input_field(last_name)
+            self.register_page.click_continue_button()
+            self.register_page.your_personal_details_form.last_name_field.error_message.get_error_message()
+        assert str(TimeoutException) in str(error.type)
+
+    @pytest.mark.parametrize('email, expected_result',
+                             [
+                                 ('y@i.ua', 'selenium.common.exceptions.TimeoutException: Message:'),
+                                 ('test@email.ua', 'selenium.common.exceptions.TimeoutException: Message:'),
+                                 ('testtesttesttesttesttesttesttes@email.ua',
+                                  'selenium.common.exceptions.TimeoutException: Message:')
+                             ]
+                             )
+    def test_email_field_valid_number_characters(self, email, expected_result):
+        with pytest.raises(TimeoutException) as error:
+            self.register_page.your_personal_details_form.email_field.clear_and_fill_input_field(email)
+            self.register_page.click_continue_button()
+            self.register_page.your_personal_details_form.email_field.error_message.get_error_message()
+        assert str(TimeoutException) in str(error.type)
+
+    @pytest.mark.parametrize('telephone, expected_result',
+                             [
+                                 ('123', 'selenium.common.exceptions.TimeoutException: Message:'),
+                                 ('1234567890987654', 'selenium.common.exceptions.TimeoutException: Message:'),
+                                 ('12345678909876543212345678909876',
+                                  'selenium.common.exceptions.TimeoutException: Message:')
+                             ]
+                             )
+    def test_telephone_field_valid_number_characters(self, telephone, expected_result):
+        with pytest.raises(TimeoutException) as error:
+            self.register_page.your_personal_details_form.telephone_field.clear_and_fill_input_field(telephone)
+            self.register_page.click_continue_button()
+            self.register_page.your_personal_details_form.telephone_field.error_message.get_error_message()
+        assert str(TimeoutException) in str(error.type)
+
+    @pytest.mark.parametrize('first_name, expected_result',
+                             [
+                                 ('', 'First Name must be between 1 and 32 characters!'),
+                                 (
+                                 'Wolfeschlegelsteinhausenbergerdoq', 'First Name must be between 1 and 32 characters!')
+                             ]
+                             )
+    def test_first_name_field_invalid_number_characters(self, first_name, expected_result):
         self.register_page.your_personal_details_form.first_name_field.clear_and_fill_input_field(first_name)
         self.register_page.click_continue_button()
         assert self.register_page.your_personal_details_form.first_name_field.error_message.get_error_message() == \
@@ -29,10 +93,10 @@ class TestAddUser:
     @pytest.mark.parametrize('last_name, expected_result',
                              [
                                  ('', 'Last Name must be between 1 and 32 characters!'),
-                                 ('111111111111111111111111111111111', 'Last Name must be between 1 and 32 characters!')
+                                 ('Enraejakavarapantiyacuppiramaniyw', 'Last Name must be between 1 and 32 characters!')
                              ]
                              )
-    def test_last_name_field_wrong_number_characters(self, last_name, expected_result):
+    def test_last_name_field_invalid_number_characters(self, last_name, expected_result):
         self.register_page.your_personal_details_form.last_name_field.clear_and_fill_input_field(last_name)
         self.register_page.click_continue_button()
         assert self.register_page.your_personal_details_form.last_name_field.error_message.get_error_message() == \
@@ -43,39 +107,20 @@ class TestAddUser:
                                  ('', 'E-Mail Address does not appear to be valid!')
                              ]
                              )
-    def test_email_field_wrong_number_characters(self, email, expected_result):
-        self.register_page.your_personal_details_form.email_field.clear_and_fill_input_field(email)
-        self.register_page.click_continue_button()
-        assert self.register_page.your_personal_details_form.email_field.error_message.get_error_message() == \
-               expected_result
+    def test_email_field_invalid_number_characters(self, email, expected_result):
+        pass
 
     @pytest.mark.parametrize('telephone, expected_result',
                              [
                                  ('12', 'Telephone must be between 3 and 32 characters!'),
-                                 ('111111111111111111111111111111111', 'Telephone must be between 3 and 32 characters!')
+                                 ('123456789098765432123456789098765', 'Telephone must be between 3 and 32 characters!')
                              ]
                              )
-    def test_telephone_field_wrong_number_characters(self, telephone, expected_result):
+    def test_telephone_field_invalid_number_characters(self, telephone, expected_result):
         self.register_page.your_personal_details_form.telephone_field.clear_and_fill_input_field(telephone)
         self.register_page.click_continue_button()
         assert self.register_page.your_personal_details_form.telephone_field.error_message.get_error_message() == \
                expected_result
-
-    # def test_input_valid_data(self):
-    #     self.register_page.your_personal_details_form.first_name_field.clear_and_fill_input_field('Maksym')
-    #     self.register_page.your_personal_details_form.last_name_field.clear_and_fill_input_field('Statham')
-    #     self.register_page.your_personal_details_form.email_field.clear_and_fill_input_field('test10@test.com.ua')
-    #     self.register_page.your_personal_details_form.telephone_field.clear_and_fill_input_field('777')
-    #
-    #     self.register_page.your_password_form.password_field.clear_and_fill_input_field('1111')
-    #     self.register_page.your_password_form.password_confirm_field.clear_and_fill_input_field('1111')
-    #
-    #     self.register_page.subscribe_radio_buttons.subscribe_to_newsletter()
-    #     self.register_page.privacy_policy_checkbox.agree_with_privacy_policy()
-    #
-    #     self.register_page.click_continue_button()
-    #
-    #     assert self.driver.title == 'Register Account'
 
     def teardown(self):
         self.driver.close()
