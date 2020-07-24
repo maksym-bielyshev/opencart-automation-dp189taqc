ccfa7f34b2b635565b7a6425cd948b10cc7cd04e
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-07-24 18:51:08 +0300

    Add logging with dictConfig

---
+++ dp189/tests/__init__.py
@@ -0,0 +1,27 @@
+"""Setting up all logs."""
+
+import logging.config
+
+logging_settings = {
+    "version": 1,
+    "formatters": {
+        "default_formatter": {
+            "format": "%(asctime)s - %(name)s - %(message)s"
+        },
+    },
+    "handlers": {
+        "handler_sample": {
+            "class": "logging.FileHandler",
+            "formatter": "default_formatter",
+            "filename": "logs/logging_sample.log"
+        },
+    },
+    "loggers": {
+        "dp189.tests.logging_sample": {
+            "handlers": ["handler_sample"],
+            "level": "DEBUG"
+        },
+    },
+}
+
+logging.config.dictConfig(logging_settings)
