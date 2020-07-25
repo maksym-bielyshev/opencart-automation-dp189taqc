474dbe8acfffb47d91357be1f249bb918896899f
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-07-25 08:54:14 +0300

    Configure 'Allure'

--- dp189/tests/conftest.py
+++ dp189/tests/conftest.py
@@ -1,13 +1,13 @@
 import pytest
 from selenium import webdriver
 
-CHROME_DRIVER = '../driver/chromedriver.exe'
+CHROME_DRIVER_WIN32 = '../driver/chromedriver.exe'
 CHROME_DRIVER_LINUX64 = '../driver/chromedriver'
 
 
 @pytest.fixture(scope="function")
 def init_driver(request):
-    driver = webdriver.Chrome(CHROME_DRIVER_LINUX64)
+    driver = webdriver.Chrome(CHROME_DRIVER_WIN32)
     request.cls.driver = driver
 
     yield driver
