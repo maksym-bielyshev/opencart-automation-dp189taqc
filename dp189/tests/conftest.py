562b3e87a3d56371473d1ecc7037b6d09a712742
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-07-25 08:00:45 +0300

    Add linux chromedriver

--- dp189/tests/conftest.py
+++ dp189/tests/conftest.py
@@ -2,11 +2,12 @@ import pytest
 from selenium import webdriver
 
 CHROME_DRIVER = '../driver/chromedriver.exe'
+CHROME_DRIVER_LINUX64 = '../driver/chromedriver'
 
 
 @pytest.fixture(scope="function")
 def init_driver(request):
-    driver = webdriver.Chrome(CHROME_DRIVER)
+    driver = webdriver.Chrome(CHROME_DRIVER_LINUX64)
     request.cls.driver = driver
 
     yield driver
