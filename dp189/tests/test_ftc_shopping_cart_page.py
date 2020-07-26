b33454fec8d652ffac73e225d016e297969afb01
Maksym Bezruchko <maksimok.dnepr@gmail.com>
2020-07-26 10:27:42 +0300

    Changed class name in constants.py from ShoppingCartPageUpdateButtonConstants to ShoppingCartConstants

--- dp189/tests/test_ftc_shopping_cart_page.py
+++ dp189/tests/test_ftc_shopping_cart_page.py
@@ -4,7 +4,7 @@ from dp189.pages.shopping_cart_page import ShoppingCartPage
 from dp189.tests.conftest import get_test_data
 from dp189.tests.base_test import BaseTest
 from dp189.routes import *
-from dp189.constants import ShoppingCartPageUpdateButtonConstants
+from dp189.constants import ShoppingCartConstants
 
 
 class TestShoppingCart(BaseTest):
@@ -24,9 +24,9 @@ class TestShoppingCart(BaseTest):
         """Positive test to check correct changing quantity of product.
         :param expected: str
         """
-        self.cart.change_product_quantity(ShoppingCartPageUpdateButtonConstants.TEST_ITEM1, '5')
+        self.cart.change_product_quantity(ShoppingCartConstants.TEST_ITEM1, '5')
 
-        assert ShoppingCartPageUpdateButtonConstants.RESULT in self.cart.catch_info_message.get_info_message()
+        assert ShoppingCartConstants.RESULT in self.cart.catch_info_message.get_info_message()
 
     @pytest.mark.parametrize('test_input,expected', get_test_data('test_data_shopping_cart_quantity.csv'))
     def test_shopping_cart_change_quantity_negative(self, test_input: str, expected: str):
@@ -34,12 +34,12 @@ class TestShoppingCart(BaseTest):
         :param test_input: str
         :param expected: str
         """
-        self.cart.change_product_quantity(ShoppingCartPageUpdateButtonConstants.TEST_ITEM1, test_input)
+        self.cart.change_product_quantity(ShoppingCartConstants.TEST_ITEM1, test_input)
 
         assert self.cart.get_text_empty_cart() == expected
 
     def test_shopping_cart_click_remove_button(self):
         """Positive test to check correct changing after remove product from shopping cart."""
-        self.cart.click_remove_product_button(ShoppingCartPageUpdateButtonConstants.TEST_ITEM1)
+        self.cart.click_remove_product_button(ShoppingCartConstants.TEST_ITEM1)
 
-        assert ShoppingCartPageUpdateButtonConstants.RESULT3 == self.cart.get_text_empty_cart()
+        assert ShoppingCartConstants.RESULT3 == self.cart.get_text_empty_cart()
