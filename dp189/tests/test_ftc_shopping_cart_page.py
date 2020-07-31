127f57ee7b5eca64f366cadbc4749fd52ab97bbf
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-07-31 11:44:51 +0300

    Prioritize tests

--- dp189/tests/test_ftc_shopping_cart_page.py
+++ dp189/tests/test_ftc_shopping_cart_page.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 from dp189.pages.product_page import ProductPage
 from dp189.pages.shopping_cart_page import ShoppingCartPage
 from dp189.tests.conftest import get_test_data
@@ -7,6 +8,7 @@ from dp189.routes import *
 from dp189.constants import ShoppingCartPageConstants
 
 
+@allure.severity(allure.severity_level.CRITICAL)
 class TestShoppingCart(BaseTest):
     def setup(self):
         self.driver.maximize_window()
@@ -20,6 +22,7 @@ class TestShoppingCart(BaseTest):
         self.cart = ShoppingCartPage(self.driver)
         self.cart.generate_products_list()
 
+    @allure.severity(allure.severity_level.MINOR)
     def test_shopping_cart_change_quantity_positive(self):
         """Positive test to check correct changing quantity of product.
         :param expected: str
@@ -28,6 +31,7 @@ class TestShoppingCart(BaseTest):
 
         assert ShoppingCartPageConstants.RESULT in self.cart.catch_info_message.get_info_message()
 
+    @allure.severity(allure.severity_level.NORMAL)
     @pytest.mark.parametrize('test_input,expected',
                              get_test_data('shopping_cart/test_data_shopping_cart_quantity.csv'))
     def test_shopping_cart_change_quantity_negative(self, test_input: str, expected: str):
@@ -39,6 +43,7 @@ class TestShoppingCart(BaseTest):
 
         assert self.cart.get_text_empty_cart() == expected
 
+    @allure.severity(allure.severity_level.MINOR)
     def test_use_coupon_code(self):
         """Positive test for checking success message display after apply coupon code."""
         self.cart.coupon_panel.open_coupon_panel()
@@ -47,12 +52,14 @@ class TestShoppingCart(BaseTest):
 
         assert ShoppingCartPageConstants.RESULT4 in self.cart.catch_info_message.get_info_message()
 
+    @allure.severity(allure.severity_level.MINOR)
     def test_shopping_cart_click_remove_button(self):
         """Positive test to check correct changing after remove product from shopping cart."""
         self.cart.click_remove_product_button(ShoppingCartPageConstants.TEST_ITEM1)
 
         assert ShoppingCartPageConstants.RESULT3 == self.cart.get_text_empty_cart()
 
+    @allure.severity(allure.severity_level.MINOR)
     def test_shopping_cart_estimate_shipping_and_taxes(self):
         """Positive test for checking success message display after fill Estimate Shipping & Taxes field."""
         self.cart.estimate_shipping_panel.open_estimate_shipping_panel()
@@ -66,16 +73,19 @@ class TestShoppingCart(BaseTest):
 
         assert ShoppingCartPageConstants.RESULT5 in self.cart.catch_info_message.get_info_message()
 
+    @allure.severity(allure.severity_level.MINOR)
     def test_checkout_button(self):
         """Positive test for checking button 'Checkout'"""
         self.cart.click_checkout_button()
         assert self.cart.get_title.get_title_page(ShoppingCartPageConstants.CHECKOUT_TITLE) == 'Checkout'
 
+    @allure.severity(allure.severity_level.MINOR)
     def test_catch_info_message_update_quantity(self):
         """Positive test for catching info message about update quantity of products in shopping cart"""
         self.cart = self.cart.change_product_quantity(ShoppingCartPageConstants.TEST_ITEM1, "2")
         assert ShoppingCartPageConstants.RESULT in self.cart.catch_info_message.get_info_message()
 
+    @allure.severity(allure.severity_level.MINOR)
     def test_total_price_of_product(self):
         """Positive test for checking total price of product after update quantity"""
         self.cart = self.cart.change_product_quantity(ShoppingCartPageConstants.TEST_ITEM1, "2")
@@ -83,6 +93,7 @@ class TestShoppingCart(BaseTest):
         total_price = self.cart.get_product_total_price(ShoppingCartPageConstants.TEST_ITEM1)
         assert ShoppingCartPageConstants.RESULT2 == total_price
 
+    @allure.severity(allure.severity_level.MINOR)
     def test_continue_button(self):
         """Positive test for checking work 'Continue' button"""
         self.cart.click_continue_shipping_button()
