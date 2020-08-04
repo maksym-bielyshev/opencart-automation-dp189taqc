c21cd381ecedc27179cca05a147ae7b0c68473f0
Diana Kardashova <kardashova.1999@gmail.com>
2020-08-04 14:20:02 +0300

    Change run.sh script

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ bash /opt/bin/entry_point.sh &
 
 # Run tests.
 cd /var/opencart-automation/dp189/tests \
-    && sudo pytest -v --alluredir=/var/opencart-automation/dp189/reports
+    && sudo pytest -v test_ftc_checkout_page.py, test_ftc_product_page.py --alluredir=/var/opencart-automation/dp189/reports
