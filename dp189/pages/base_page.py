b3e9443be440f26f12af61a9aeefe5c43256c175
Tanya-Kr <tanya.krug7@gmail.com>
2020-08-06 17:27:26 +0300

    Tests fixing

--- dp189/pages/base_page.py
+++ dp189/pages/base_page.py
@@ -1,4 +1,7 @@
+from telnetlib import EC
+
 from selenium.webdriver import Remote
+from selenium.webdriver.support.wait import WebDriverWait
 
 from ..components import BasePageNavBarComponent, DropdownComponent, InputFieldComponent, ShopCartButtonComponent
 from ..components import CatchMessageComponent, CatchPageTitleComponent
@@ -18,20 +21,24 @@ class BasePage:
         self.main_menu_phones_pdas = DropdownComponent(driver, LocatorsBasePageMainMenu.PHONES_PDAS)
         self.main_menu_cameras = DropdownComponent(driver, LocatorsBasePageMainMenu.CAMERAS)
         self.main_menu_mp3players = DropdownComponent(driver, LocatorsBasePageMainMenu.MP3_PLAYERS)
-        self.my_account = driver.find_element(*LocatorsBasePageNavBar.MY_ACCOUNT)
         self.your_store_link = driver.find_element(*LocatorYourStoreLink.YOUR_STORE)
         self.search_field = InputFieldComponent(driver, LocatorBasePageSearch.SEARCH_FIELD)
         self.shop_cart_button = ShopCartButtonComponent(driver)
         self.catch_info_message = CatchMessageComponent(self._driver)
         self.get_title = CatchPageTitleComponent(self._driver)
 
+    def wait_load_account_load(self):
+        return self._driver.find_element(*LocatorsBasePageNavBar.MY_ACCOUNT)
+
     def click_account_and_go_to_login(self):
-        self.my_account.click()
-        self.my_account.find_element(*LocatorsBasePageNavBar.LOGIN).click()
+        my_account_button = self.wait_load_account_load()
+        my_account_button.click()
+        my_account_button.find_element(*LocatorsBasePageNavBar.LOGIN).click()
     
     def click_account_and_go_to_register(self):
-        self.my_account.click()
-        self.my_account.find_element(*LocatorsBasePageNavBar.REGISTER).click()
+        my_account_button = self.wait_load_account_load()
+        my_account_button.click()
+        my_account_button.find_element(*LocatorsBasePageNavBar.REGISTER).click()
 
     def find_element(self, locator):
         self._driver.implicitly_wait(5)
