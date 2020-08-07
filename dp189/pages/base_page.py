dcda710ccc0059e21e52d8dd2bc7c7810094c38c
Danya1998 <yurchenkodanil1998@gmail.com>
2020-08-07 13:06:24 +0300

    refactoring base_page.py

--- dp189/pages/base_page.py
+++ dp189/pages/base_page.py
@@ -1,10 +1,8 @@
 from selenium.webdriver import Remote
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from ..components import BasePageNavBarComponent, DropdownComponent, InputFieldComponent, ShopCartButtonComponent
-from ..components import CatchMessageComponent, CatchPageTitleComponent
-from dp189.locators import LocatorsBasePageMainMenu, LocatorYourStoreLink, LocatorBasePageSearch
-from ..locators import LocatorsBasePageNavBar
+from dp189.components import InputFieldComponent, ShopCartButtonComponent
+from dp189.components import CatchMessageComponent, CatchPageTitleComponent, MainMenuComponent
+from dp189.locators import LocatorBasePageSearch
+from dp189.locators import LocatorsBasePageNavBar
 
 
 class BasePage:
@@ -16,17 +14,10 @@ class BasePage:
         :param driver: Remote.
         """
         self._driver = driver
-        self.top_nav_bar = BasePageNavBarComponent(driver)
-        self.main_menu_desktops = DropdownComponent(driver, LocatorsBasePageMainMenu.DESKTOPS)
-        self.main_menu_laptops_notebooks = DropdownComponent(driver, LocatorsBasePageMainMenu.LAPTOPS_NOTEBOOKS)
-        self.main_menu_components = DropdownComponent(driver, LocatorsBasePageMainMenu.COMPONENTS)
-        self.main_menu_tablets = DropdownComponent(driver, LocatorsBasePageMainMenu.TABLETS)
-        self.main_menu_software = DropdownComponent(driver, LocatorsBasePageMainMenu.SOFTWARE)
-        self.main_menu_phones_pdas = DropdownComponent(driver, LocatorsBasePageMainMenu.PHONES_PDAS)
-        self.main_menu_cameras = DropdownComponent(driver, LocatorsBasePageMainMenu.CAMERAS)
-        self.main_menu_mp3players = DropdownComponent(driver, LocatorsBasePageMainMenu.MP3_PLAYERS)
+        # self.top_nav_bar = BasePageNavBarComponent(driver)
         self.my_account = driver.find_element(*LocatorsBasePageNavBar.MY_ACCOUNT)
-        self.your_store_link = driver.find_element(*LocatorYourStoreLink.YOUR_STORE)
+        self.main_menu = MainMenuComponent(driver)
+        # self.your_store_link = driver.find_element(*LocatorYourStoreLink.YOUR_STORE)
         self.search_field = InputFieldComponent(driver, LocatorBasePageSearch.SEARCH_FIELD)
         self.shop_cart_button = ShopCartButtonComponent(driver)
         self.catch_info_message = CatchMessageComponent(self._driver)
@@ -57,3 +48,6 @@ class BasePage:
         """
         self._driver.implicitly_wait(8)
         return self._driver.find_element(*locator)
+
+
+
