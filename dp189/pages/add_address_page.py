489400114a6f2c78508ffbd3b539cf5100ab1675
Diana Kardashova <kardashova.1999@gmail.com>
2020-08-07 13:21:34 +0300

    Add docstrings to add_address_page.py, conftest.py, locators.py, test_ftc_product_page.py.

--- dp189/pages/add_address_page.py
+++ dp189/pages/add_address_page.py
@@ -6,25 +6,25 @@ from selenium.webdriver import Remote
 
 class AddAddressPage(BasePage):
     """Description of the basic methods of working with 'Add Address' Page."""
-    def __init__(self, driver: Remote):
-        super().__init__(driver)
+    def __init__(self, driver: Remote) -> None:
         """Initialize objects to work with this page.
-        
+
         :param driver: Remote
+        :return: None
         """
+        super().__init__(driver)
         self.right_menu = RegisterPageRightMenuComponent(driver)
         self.address_content = self._driver.find_element(*LocatorsAddAddressPage.ADDRESS_CONTENT)
         self.address_fields = AddAddressComponent(self._driver, self.address_content)
         self.default_address_radiobutton = RadioButtonComponent(self._driver,
                                                                 LocatorsAddAddressPage.DEFAULT_ADDRESS_RADIO_CONTAINER)
 
-    def click_back_button(self) -> object:
+    def click_back_button(self) -> None:
         """Click 'Back' button to return to 'Address Book' page.
 
-        :return: AddressBookPage
+        :return: None
         """
         self._driver.find_element(*LocatorsAddAddressPage.BACK_BUTTON).click()
-        # return AddressBookPage(self._driver)
 
     def click_continue_button(self) -> None:
         """Click 'Continue' button to return to 'Address Book' page if all filled data is valid or return to
