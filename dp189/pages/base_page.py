68149235ee9403433f420fc1a311cea6084685e5
Danya1998 <yurchenkodanil1998@gmail.com>
2020-08-07 11:27:23 +0300

    refactoring base_page.py

--- dp189/pages/base_page.py
+++ dp189/pages/base_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver import Remote
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from ..components import BasePageNavBarComponent, DropdownComponent, InputFieldComponent, ShopCartButtonComponent
 from ..components import CatchMessageComponent, CatchPageTitleComponent
 from dp189.locators import LocatorsBasePageMainMenu, LocatorYourStoreLink, LocatorBasePageSearch
@@ -7,7 +8,13 @@ from ..locators import LocatorsBasePageNavBar
 
 
 class BasePage:
+    """Base page class."""
+
     def __init__(self, driver: Remote):
+        """Initialize main class.
+
+        :param driver: Remote.
+        """
         self._driver = driver
         self.top_nav_bar = BasePageNavBarComponent(driver)
         self.main_menu_desktops = DropdownComponent(driver, LocatorsBasePageMainMenu.DESKTOPS)
@@ -26,13 +33,27 @@ class BasePage:
         self.get_title = CatchPageTitleComponent(self._driver)
 
     def click_account_and_go_to_login(self):
+        """Method which click on link and go to Login page.
+
+        :return: None
+        """
         self.my_account.click()
         self.my_account.find_element(*LocatorsBasePageNavBar.LOGIN).click()
-    
+
     def click_account_and_go_to_register(self):
+        """Method which click on link and go to Register page.
+
+        :return: None
+        """
         self.my_account.click()
         self.my_account.find_element(*LocatorsBasePageNavBar.REGISTER).click()
 
-    def find_element(self, locator):
-        self._driver.implicitly_wait(5)
+    def find_element(self, locator: tuple):
+        """Method which find element on page.
+
+
+        :param: Tuple[str, str]
+        :return: WebElement
+        """
+        self._driver.implicitly_wait(8)
         return self._driver.find_element(*locator)
